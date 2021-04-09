65c03e6db4154af9f002b5ac0f56dc21f3b4fed9
barbodnavabi <barbodnvb@gmail.com>
2021-04-09 08:15:14 -0700

    margin for my question text

--- first_app/lib/question.dart
+++ first_app/lib/question.dart
@@ -8,6 +8,7 @@ class Question extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: EdgeInsets.all(15),
       child: Text(
         questionText,
         style: TextStyle(fontSize: 29),
